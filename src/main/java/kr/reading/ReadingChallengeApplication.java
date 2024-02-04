f56db3e13401582a8cf4b8feb4cfd26c744e9ca1
kmindev <kangkm0324@gmail.com>
2024-02-04 15:56:51 +0900

    #11 - 스프링 부트 프로젝트 생성
    * 디펜던시 추가(Spring Web, Spring Data JPA, Spring Security, Lombok, mysql connector)
    * db 및 jpa 설정 정보

---
+++ src/main/java/kr/reading/ReadingChallengeApplication.java
@@ -0,0 +1,13 @@
+package kr.reading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/reading/ReadingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.reading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
