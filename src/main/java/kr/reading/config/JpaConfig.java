70dbcd54e7f6d9143102c3bb90b04191da51e287
kmindev <kangkm0324@gmail.com>
2024-04-19 21:13:22 +0900

    #19 - 시큐리티 구현으로 인한 'auditorAware' 대응

--- src/main/java/kr/reading/config/JpaConfig.java
+++ src/main/java/kr/reading/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package kr.reading.config;
 
+import kr.reading.security.PrincipalDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,6 +17,11 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<Long> auditorAware() {
-        return () -> Optional.of(1L); // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될 때 수정이 필요
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(PrincipalDetails.class::cast)
+                .map(PrincipalDetails::id);
     }
 }
