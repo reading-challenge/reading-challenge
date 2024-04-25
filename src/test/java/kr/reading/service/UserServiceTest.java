39285fe41dcdc33be54940edd716f5979a245497
kmindev <kangkm0324@gmail.com>
2024-04-25 20:08:01 +0900

    #22 - 회원 탈퇴 서비스, 컨트롤러 테스트
    * 회원가입 서비스 테스트에서 테스트가 잘못 작성되어 있어서 수정 willThrow -> willReturn
    * 회원탈퇴 서비스, 컨트롤러 로직 테스트 완료

--- src/test/java/kr/reading/service/UserServiceTest.java
+++ src/test/java/kr/reading/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package kr.reading.service;
 import kr.reading.domain.User;
 import kr.reading.dto.UserDto;
 import kr.reading.global.exception.EmailExistsException;
+import kr.reading.global.exception.InactiveUserException;
 import kr.reading.global.exception.NicNameExistsException;
 import kr.reading.global.exception.UserIdExistsException;
 import kr.reading.repository.UserRepository;
@@ -20,8 +21,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -31,6 +31,37 @@ class UserServiceTest {
     @InjectMocks private UserService sut;
     @Mock private UserRepository userRepository;
     @Mock private PasswordEncoder passwordEncoder;
+
+    @DisplayName("탈퇴하지 않은 회원 id를 입력하면, entity 반환한다.")
+    @Test
+    void givenId_whenFindActiveUserById_thenReturnsEntity() throws Exception {
+        // Given
+        User user = createUser();
+        given(userRepository.findByIdAndDeletedAtIsNull(anyLong())).willReturn(Optional.of(user));
+
+        // When
+        User resultUser = sut.findActiveUserById(user.getId());
+
+        // Then
+        assertThat(resultUser.getId()).isEqualTo(user.getId());
+        then(userRepository).should().findByIdAndDeletedAtIsNull(user.getId());
+    }
+
+    @DisplayName("탈퇴한 회원 id를 입력하면, 예외가 발생한다.")
+    @Test
+    void givenInactiveId_whenFindActiveUserById_thenThrowsException() throws Exception {
+        // Given
+        given(userRepository.findByIdAndDeletedAtIsNull(anyLong())).willReturn(Optional.ofNullable(null));
+
+        // When
+        InactiveUserException exception = assertThrows(InactiveUserException.class,
+                () -> sut.findActiveUserById(1L)
+        );
+
+        // Then
+        assertThat(exception).isInstanceOf(InactiveUserException.class);
+        then(userRepository).should().findByIdAndDeletedAtIsNull(1L);
+    }
 
     @DisplayName("회원가입 정보를 입력하면, 회원가입이 완료된다.")
     @Test
@@ -61,7 +92,8 @@ class UserServiceTest {
     void givenExistsUserId_whenSignup_thenThrowException() {
         // Given
         UserDto userDto = createUserDto();
-        given(userRepository.findByUserId(anyString())).willThrow(new UserIdExistsException());
+        User user = createUser();
+        given(userRepository.findByUserId(anyString())).willReturn(Optional.of(user));
 
         // When
         UserIdExistsException exception = assertThrows(UserIdExistsException.class,
@@ -78,9 +110,29 @@ class UserServiceTest {
     void givenExistsEmail_whenSignup_thenThrowException() {
         // Given
         UserDto userDto = createUserDto();
+        User user = createUser();
+        given(userRepository.findByUserId(anyString())).willReturn(Optional.ofNullable(null));
+        given(userRepository.findByEmail(anyString())).willReturn(Optional.ofNullable(user));
+
+        // When
+        EmailExistsException exception = assertThrows(EmailExistsException.class,
+                () -> sut.signup(userDto)
+        );
+
+        // Then
+        assertThat(exception).isInstanceOf(EmailExistsException.class);
+        then(passwordEncoder).shouldHaveNoInteractions();
+    }
+
+    @DisplayName("중복된 닉네임을 입력하면, 예외가 발생한다.")
+    @Test
+    void givenExistsNickname_whenSignup_thenThrowException() {
+        // Given
+        UserDto userDto = createUserDto();
+        User user = createUser();
         given(userRepository.findByUserId(anyString())).willReturn(Optional.ofNullable(null));
         given(userRepository.findByEmail(anyString())).willReturn(Optional.ofNullable(null));
-        given(userRepository.findByNickname(anyString())).willThrow(new NicNameExistsException());
+        given(userRepository.findByNickname(anyString())).willReturn(Optional.ofNullable(user));
 
         // When
         NicNameExistsException exception = assertThrows(NicNameExistsException.class,
@@ -92,22 +144,35 @@ class UserServiceTest {
         then(passwordEncoder).shouldHaveNoInteractions();
     }
 
-    @DisplayName("중복된 닉네임을 입력하면, 예외가 발생한다.")
+    @DisplayName("탈퇴하지 않은 회원 id를 입력하면, soft delete 한다.")
     @Test
-    void givenExistsNickname_whenSignup_thenThrowException() {
+    void givenId_whenDeleteUser_thenSoftDeleteUser() {
         // Given
-        UserDto userDto = createUserDto();
-        given(userRepository.findByUserId(anyString())).willReturn(Optional.ofNullable(null));
-        given(userRepository.findByEmail(anyString())).willThrow(new EmailExistsException());
+        User user = createUser();
+        given(userRepository.findByIdAndDeletedAtIsNull(anyLong())).willReturn(Optional.ofNullable(user));
 
         // When
-        EmailExistsException exception = assertThrows(EmailExistsException.class,
-                () -> sut.signup(userDto)
+        UserDto userDto = sut.deleteUser(user.getId());
+
+        // Then
+        assertThat(userDto.deletedAt()).isNotNull();
+        then(userRepository).should().findByIdAndDeletedAtIsNull(anyLong());
+    }
+
+    @DisplayName("탈퇴하거나 없는 회원 id를 입력하면, 예외를 발생한다.")
+    @Test
+    void givenInactiveId_whenDeleteUser_thenThrowsException() {
+        // Given
+        given(userRepository.findByIdAndDeletedAtIsNull(anyLong())).willReturn(Optional.ofNullable(null));
+
+        // When
+        InactiveUserException exception = assertThrows(InactiveUserException.class,
+                () -> sut.deleteUser(1L)
         );
 
         // Then
-        assertThat(exception).isInstanceOf(EmailExistsException.class);
-        then(passwordEncoder).shouldHaveNoInteractions();
+        assertThat(exception).isInstanceOf(InactiveUserException.class);
+        then(userRepository).should().findByIdAndDeletedAtIsNull(anyLong());
     }
 
     private User createUser() {
