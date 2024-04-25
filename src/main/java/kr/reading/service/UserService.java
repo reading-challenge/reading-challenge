38fa30ea63dca045509d74aca6e8f559478e4cb7
kmindev <kangkm0324@gmail.com>
2024-04-25 20:05:55 +0900

    #22 - 회원 탈퇴 기능 구현
    * 회원 탈퇴시 soft delete를 적용함.
    * 회원탈퇴 완료시 클라이언트 쿠키와 세션 정보를 지워줌.

--- src/main/java/kr/reading/service/UserService.java
+++ src/main/java/kr/reading/service/UserService.java
@@ -2,19 +2,25 @@ package kr.reading.service;
 
 import kr.reading.domain.User;
 import kr.reading.dto.UserDto;
-import kr.reading.global.exception.EmailExistsException;
-import kr.reading.global.exception.NicNameExistsException;
-import kr.reading.global.exception.UserIdExistsException;
+import kr.reading.global.exception.*;
 import kr.reading.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true)
+    public User findActiveUserById(Long id) {
+        return userRepository.findByIdAndDeletedAtIsNull(id)
+                .orElseThrow(InactiveUserException::new);
+    }
 
     public UserDto signup(UserDto dto) {
         userRepository.findByUserId(dto.userId()).ifPresent(user -> {
@@ -34,5 +40,11 @@ public class UserService {
         User registerdUser = userRepository.save(entity);
 
         return UserDto.from(registerdUser);
+    }
+
+    public UserDto deleteUser(Long id) {
+        User user = findActiveUserById(id);
+        user.delete();
+        return UserDto.from(user);
     }
 }
