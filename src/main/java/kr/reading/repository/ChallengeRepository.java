d84ba07afb611febe4cdb0bcfe6a32df154fc2e9
kmindev <kangkm0324@gmail.com>
2024-06-25 23:19:02 +0900

    #29 - 챌린지 조회 기능 구현
    soft delete를 적용할 에정이기 때문에 이를 고려해서 쿼리를 작성함.

--- src/main/java/kr/reading/repository/ChallengeRepository.java
+++ src/main/java/kr/reading/repository/ChallengeRepository.java
@@ -1,7 +1,11 @@
 package kr.reading.repository;
 
 import kr.reading.domain.Challenge;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+    Page<Challenge> findAllByDeletedAtIsNull(Pageable pageable);
 }
--- src/main/java/kr/reading/service/ChallengeService.java
+++ src/main/java/kr/reading/service/ChallengeService.java
@@ -4,6 +4,8 @@ import kr.reading.domain.Challenge;
 import kr.reading.dto.ChallengeDto;
 import kr.reading.repository.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,10 @@ public class ChallengeService {
     public ChallengeDto createChallenge(ChallengeDto dto) {
         Challenge challenge = dto.toEntity();
         return ChallengeDto.from(challengeRepository.save(challenge));
+    }
+
+    public Page<ChallengeDto> getChallenges(Pageable pageable) {
+        return challengeRepository.findAllByDeletedAtIsNull(pageable).map(ChallengeDto::from);
     }
 
 }
