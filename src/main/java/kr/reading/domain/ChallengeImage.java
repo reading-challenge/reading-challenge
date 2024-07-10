1bd0e198ce3eaf64f607dcecd93ea29aad30156b
kmindev <kangkm0324@gmail.com>
2024-07-10 20:40:53 +0900

    #32 - 챌린지 이미지 엔티티 및 테스트 용 데이터 생성
    챌린지 이미지 엔티티 및 jpa 레포지토리 생성
    챌린지 이미지 테스트 용 데이터 50개 생성

---
+++ src/main/java/kr/reading/domain/ChallengeImage.java
@@ -0,0 +1,39 @@
+package kr.reading.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ChallengeImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imgSrc; // 이미지 경로
+
+    @JoinColumn(name = "challengeId")
+    @ManyToOne(optional = false)
+    private Challenge challenge;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChallengeImage that)) return false;
+        return this.getId() != null && this.getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
+
+}
